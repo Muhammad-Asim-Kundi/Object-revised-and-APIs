fad5b792764c1ab1b97fea01946c38c160e5dc9e
Muhammad Asim Kundi <muhammadasimkundi@gmail.com>
2023-09-05 20:12:01 +0500

    practiced drink api sometimes

--- class26-materials/objects-shots/js/main.js
+++ class26-materials/objects-shots/js/main.js
@@ -1,5 +1,5 @@
 //The user will enter a cocktail. Get a cocktail name, photo, and instructions and place them in the DOM
-document.querySelector('button').addEventListener('click',getDrink)
+// document.querySelector('button').addEventListener('click',getDrink)
 
 function getDrink(){
     let drink = document.querySelector('input').value
@@ -19,3 +19,5 @@ function getDrink(){
 })
 
 }
+
+document.querySelector('button').addEventListener('click',getDrink)

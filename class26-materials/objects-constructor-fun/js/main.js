b1cf4225914a40879083bc26473052b261119450
Muhammad Asim Kundi <muhammadasimkundi@gmail.com>
2023-09-05 19:55:23 +0500

    some comments to the apis

--- class26-materials/objects-constructor-fun/js/main.js
+++ class26-materials/objects-constructor-fun/js/main.js
@@ -1,5 +1,5 @@
 //Create a constructor with 4 properties and 3 methods
-function Car (make, model , color, door){
+function MakeCar (make, model , color, door){
     this.make= make
     this.model = model
     this.color = color
@@ -19,8 +19,35 @@ function Car (make, model , color, door){
 
 }
 
-toyota = new Car('toyota', 'prius-23' , 'silver' , 4)
+let toyota = new MakeCar('toyota', 'prius-23' , 'silver' , 4)
 
-tesla = new Car('tesla', 'X-2022' , 'white' , 2)
+let tesla = new MakeCar('tesla', 'X-2022' , 'white' , 2)
 
-lomborghinie = new Car('lomborghinie' , ['R-20','A-23'] , 'red' , 2)
+let lomborghinie = new MakeCar('lomborghinie' , ['R-20','A-23'] , 'red' , 2)
+
+
+// the new look or the new syntax 
+
+
+class Car{
+    constructor(make, model , color, door){
+        this.make= make
+        this.model = model
+        this.color = color
+        this.door = door 
+    }
+
+    driveType(){
+        alert(`we can drive ${this.make} without a driver`)
+    }
+
+    speed(){
+        alert(`${this.make} ${this.model} can go up to 260kmh`)
+    }
+
+    fuelAverage(){
+        alert(`${this.make} has ${this.door} doors and ${this.color} color is giving me good fuelAverage of 20km/L`)
+    }
+}
+
+let honda = new MakeCar('Honda', 'Civic-23' , 'Black' , 4)
--- class26-materials/objects-dog-photos/js/main.js
+++ class26-materials/objects-dog-photos/js/main.js
@@ -1,1 +1,10 @@
 //Get a dog photo from the dog.ceo api and place the photo in the DOM
+fetch("https://dog.ceo/api/breeds/image/random")
+.then(res => res.json())
+.then(data =>{
+    document.querySelector('img').src = data.message
+})
+.then(err =>{
+    console.log(`error ${err}`)
+})
+
